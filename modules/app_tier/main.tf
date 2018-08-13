9b578964a69b7d3f3d627e21a3c2abdddfd1d75e
JRenz12 <jrenzulli@spartglobal.com>
2018-08-13 15:15:35 +0100

    ASG spinning up 3 random instances and a standard one, aim is to spin up 3 of the same instances

--- modules/app_tier/main.tf
+++ modules/app_tier/main.tf
@@ -10,7 +10,36 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
+## Declare the data source
+data "aws_availability_zones" "available" {}
 
+    ## SUBNETS
+resource "aws_subnet" "app_subnet_1a" {
+  vpc_id     = "${var.vpc_id}"
+  cidr_block = "10.10.0.0/24"
+  availability_zone = "eu-west-1a"
+  tags {
+    Name = "subnet-app-project4"
+  }
+}
+
+resource "aws_subnet" "app_subnet_1b" {
+vpc_id     = "${var.vpc_id}"
+cidr_block = "10.10.1.0/24"
+availability_zone = "eu-west-1b"
+tags {
+  Name = "subnet-app-project4"
+}
+}
+
+resource "aws_subnet" "app_subnet_1c" {
+  vpc_id     = "${var.vpc_id}"
+  cidr_block = "10.10.2.0/24"
+  availability_zone = "eu-west-1c"
+  tags {
+    Name = "subnet-app-project4"
+  }
+}
 
 
     ## ROUTE TABLE
@@ -28,7 +57,7 @@ resource "aws_route_table" "app_route_table" {
 }
 
 resource "aws_route_table_association" "app_route_association" {
-  subnet_id      = "${aws_subnet.app_subnet.id}"
+  subnet_id      = "${aws_subnet.app_subnet_1a.id}"
   route_table_id = "${aws_route_table.app_route_table.id}"
 }
 
@@ -42,16 +71,6 @@ resource "aws_internet_gateway" "app_intenet_gateway" {
   }
 }
 
-
-      ## SUBNET
-resource "aws_subnet" "app_subnet" {
-  vpc_id     = "${var.vpc_id}"
-  cidr_block = "10.10.1.0/24"
-  availability_zone = "eu-west-1a"
-  tags {
-    Name = "subnet-app-manvir"
-  }
-}
 
 
     ## SECURITY GROUP
@@ -76,14 +95,55 @@ resource "aws_security_group" "app_security_group" {
 
 
     ## INSTANCE
-resource "aws_instance" "app_instance_1a" {
+resource "aws_instance" "app_instance" {
   ami = "${var.app_ami_id}"
   instance_type = "t2.micro"
   user_data = "${var.user_data}"
-  subnet_id = "${aws_subnet.app_subnet.id}"
+  subnet_id = "${aws_subnet.app_subnet_1a.id}"
   security_groups = ["${aws_security_group.app_security_group.id}"]
   associate_public_ip_address = true
   tags {
     Name = "app-instance-1a"
   }
+}
+
+  ## AUTO SCALING GROUP
+
+resource "aws_launch_template" "app_launch_template" {
+  name_prefix = "Project4-launch"
+  image_id = "ami-c2b8bfbb"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "app_auto_scaling" {
+  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  desired_capacity = 3
+  max_size = 4
+  min_size = 3
+
+  launch_template = {
+    id = "${aws_launch_template.app_launch_template.id}"
+    version = "$$Latest"
+  }
+}
+
+    ## ELB
+resource "aws_elb" "elb_app" {
+  name = "elb-app"
+  security_groups = ["${aws_security_group.app_security_group.id}"]
+  subnets = ["${aws_subnet.app_subnet_1a.id}", "${aws_subnet.app_subnet_1b.id}", "${aws_subnet.app_subnet_1c.id}"]
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:8080/"
+  }
+    listener {
+      lb_port = 80
+      lb_protocol = "http"
+      instance_port = "8080"
+      instance_protocol = "http"
+    }
 }
