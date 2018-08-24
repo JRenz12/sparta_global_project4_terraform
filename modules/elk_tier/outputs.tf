d97f1e53d3e9959392829bb35fba56d10332ed3f
manibrar <manibrar@github.com>
2018-08-24 13:10:05 +0100

    working

--- modules/elk_tier/outputs.tf
+++ modules/elk_tier/outputs.tf
@@ -1,5 +1,5 @@
 output "kibana_url" {
-  value       = "http://${aws_instance.elk_manvir.public_ip}:5601"
+  value       = "http://${aws_instance.elk_manvir.private_dns}:5601"
   description = "URL to your ELK server's Kibana web page"
 }
 
